c45b55249da5ec5c6f662aacc446632b8bcd6b0e
pooja97 <poojayadav2124@gmail.com>
2024-01-03 22:41:12 -0500

    added mongodb consumer

---
+++ scripts/Kafka_consumer_mongodb.py
@@ -0,0 +1,96 @@
+from confluent_kafka import Consumer, KafkaError
+from pymongo import MongoClient
+import logging
+import time
+
+
+logging.basicConfig(level = logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+logger = logging.getLogger(__name__)
+
+class MongoDBConnector:
+    def __init__(self, mongodb_uri, database_name, collection_name):
+        self.client = MongoClient(mongodb_uri)
+        self.db = self.client[database_name]
+        self.collection_name = collection_name
+
+    def create_collection(self):
+        #check if collection already exists 
+        if self.collection_name not in self.db.list_collection_names():
+            self.db.create_collection(self.collection_name)
+            logger.info(f"Create collection:{self.collection_name}")
+        else:
+            logger.warning(f"collection {self.collection_name} already exists")
+
+
+    def insert_data(self, email, otp):
+        document = {
+            "email" : email,
+            "otp" : otp 
+        }
+        self.db[self.collection_name].insert_one(document)
+
+    def close(self):
+        self.client.close() 
+
+
+mongodb_uri = 'mongodb://root:root@mongo:27017/'
+database_name = 'email_database'
+collection_name = 'email_collection'
+mongodb_connector = MongoDBConnector(mongodb_uri, database_name,collection_name)
+
+class KafkaConsumerWrapperMongoDB:
+    def __init__(self, kafka_config, topics):
+        self.consumer = Consumer(kafka_config)
+        self.consumer.subscribe(topics)
+    
+    def consume_and_insert_messages(self):
+        start_time = time.time()
+        try:
+            while True:
+                elapsed_time = time.time() - start_time
+                if elapsed_time >= 30:
+                    break 
+                msg = self.consumer.poll(1.0)
+                if msg is None:
+                    continue 
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        logger.info("Reached end of partition")
+                    else:
+                        logger.warning('Error: {}'.format(msg.error()))
+                else:
+                    email = msg.key().decode('utf-8')
+                    otp = msg.value().decode('utf-8')
+                    existing_document = self.db[self.collection_name].find_one({"email":email,"otp":otp})
+                    if existing_document:
+                        logger.warning(f"Document with Email = {email}, otp = {otp} already exists in the collection")
+                    else:
+                        mongodb_connector.insert_data(email,otp)
+                        logger.info(f'Received and inserted: Email ={email}, OTP ={otp}')
+        except: 
+            logger.info("Recieve Keyboard Interrup. Closing consumer")
+
+        finally:
+            mongodb_connector.close()
+        
+        def close(self):
+            self.consumer.close() 
+
+def kafka_consumer_mongodb_main():
+    mongodb_connector.create_collection()
+    kafka_config={
+        'bootstrap_servers':'kafka1:19092, kafka2:19093, kafka3:19094',
+        'group.id':'consumer_group',
+        'auto.offset.reset':'earliest'
+    }
+    topics = ['email_topic']
+    kafka_consumer = KafkaConsumerWrapperMongoDB(kafka_config,topics)
+    kafka_consumer.consume_and_insert_messages()
+
+    
+
+        
+
+
+
+    
